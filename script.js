6e4366f2412e0d46e199290ce0253cfc0086bed8
unknown <thericardoguilherme@gmail.com>
2023-01-30 19:14:18 -0300

    JavaScript + HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const saudacao = document.getElementById("saudacao");
+
+// getElementById - Deve ser no singular, pois o Id só retorna um elemento.
+
+
+saudacao.innerText = "Bom Noite!!!"
+
+
+console.log(saudacao)
